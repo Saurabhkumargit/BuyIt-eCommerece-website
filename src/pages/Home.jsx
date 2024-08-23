47aef6dee6494993e441580d08a457a2ed5882ce
Saurabh Kumar Tiwari <147151779+Saurabhkumargit@users.noreply.github.com>
2024-08-24 02:58:05 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Collection from '../components/Collection'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
