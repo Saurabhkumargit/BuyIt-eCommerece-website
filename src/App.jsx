9408bf35167fbecbb6891c53fa89492558a6f0e7
Saurabh Kumar Tiwari <147151779+Saurabhkumargit@users.noreply.github.com>
2024-08-24 02:46:59 +0530

    Update App.jsx
    edited the import Home option in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Store from './pages/store';
 import Product from './pages/product';
 import PlaceOrder from './pages/placeorder';
