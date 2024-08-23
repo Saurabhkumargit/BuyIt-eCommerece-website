f7b4c40473bc0d63a9959de6b739feed07173117
Saurabh Kumar Tiwari <147151779+Saurabhkumargit@users.noreply.github.com>
2024-08-24 02:53:44 +0530

    Update App.jsx
    updated the cases in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Store from './pages/store';
-import Product from './pages/product';
-import PlaceOrder from './pages/placeorder';
-import Orders from './pages/orders';
-import Login from './pages/login';
-import Contact from './pages/contact';
+import Store from './pages/Store';
+import Product from './pages/Product';
+import PlaceOrder from './pages/Placeorder';
+import Orders from './pages/Orders';
+import Login from './pages/Login';
+import Contact from './pages/Contact';
 import Cart from './pages/Cart';
-import About from './pages/about';
-import Navbar from './components/navbar';
+import About from './pages/About';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 
